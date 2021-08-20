9cbaba94ec861fde8733cf0d3de2098949238689
philippsoder <philippsoeder@gmx.de>
2021-08-20 16:48:24 +0200

    Complete Task 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=phlppsdr/udacity_app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityapp --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl wait --for=condition=ready pod udacityapp
+kubectl port-forward udacityapp 8000:80
