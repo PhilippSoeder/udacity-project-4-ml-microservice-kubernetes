8d04184ef3f2add1569d8296ff3fd7b77d8a20d7
philippsoder <philippsoeder@gmx.de>
2021-08-20 16:17:52 +0200

    Complete Task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=phlppsdr/udacity_app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username phlppsdr
+docker tag udacity_app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
